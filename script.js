aeea7f6c349b7fb9babc2ddc2bc1573983e3c05a
Ben Hernandez <benjammin234@gmail.com>
2019-12-19 20:04:33 -0800

    initial and hopefully final commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+$(function() {
+    console.log('App loaded. Waiting for submit.');
+    watchForm();
+});
+
+function watchForm() {
+    $('#dog-num').submit(event => {
+        event.preventDefault();
+        let userNumInput = $('#numDogs').val();
+        getDogImages(userNumInput);
+    });
+}
+
+function getDogImages(n) {
+        fetch(`https://dog.ceo/api/breeds/image/random/${n}`)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+        .catch(error => alert("Apologies, one of the puppies had an accident and we're cleaning it up. Please try again in a few moments."));
+    
+}
